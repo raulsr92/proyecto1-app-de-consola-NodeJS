e718eef537fe79b311a49fed0fd1047434cd8549
Raul Sanchez R. <raul.sr92@gmail.com>
2026-07-16 12:58:33 -0500

    Clase 79 Opciones de Yargs

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,38 @@
 import { yarg } from "./config/plugins/yargs.plugin";
 
 
-console.log(process.argv)
-console.log("-------------------------------")
-console.log(yarg)
+//console.log(process.argv)
+//console.log("-------------------------------")
+//console.log(yarg)
+
+(async() =>{
+
+    await main();
+
+    console.log("Fin del programa 🚨")
+
+})()
 
 
+async function main() {
+    console.log("Main Ejecutado📢🚀")
+    console.log(yarg)   
 
+    //Desestructurar
 
+    const {b, base}= yarg;
+    console.log(b)
+    console.log(base)
+
+}
 
 
 /*
-
 console.log(process.argv)
 console.log(process.argv.length)
 
 //Desestructurar array de argumentos
 
 const [tsnode,app,...argumentos] = process.argv
-
 console.log(argumentos)
 */
--- src/config/plugins/yargs.plugin.ts
+++ src/config/plugins/yargs.plugin.ts
@@ -4,4 +4,22 @@ import { hideBin } from 'yargs/helpers';
 
 
 export const yarg = yargs(hideBin(process.argv))
+    .option('b',{
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+        describe: 'Multiplication table base'
+    })
+    .option('l',{
+        alias: 'limit',
+        type: 'number',
+        default: 10,
+        describe: 'Multiplication table limit'
+    })
+    .option('s',{
+        alias: 'show',
+        type: 'boolean',
+        default: false,
+        describe: 'Show Multiplication table'
+    })
     .parseSync()
