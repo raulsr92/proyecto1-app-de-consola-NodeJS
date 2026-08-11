49365f092996784a32e71b8612babdc1842ea9f9
Raul Sanchez R. <raul.sr92@gmail.com>
2026-08-11 12:29:11 -0500

    Clase 96 Pruebas - SaveFile UseCase custom values

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -2,17 +2,31 @@ import { SaveFile } from "./save-file.use-case"
 import fs from "fs";
 
 describe('Test in UseCase Save File',()=>{
+
+    const customOptions = {
+        fileContent: 'custom content',
+        fileDestination:'custom-outputs',
+        fileName: 'custom-table-name'
+    }
+
+    const customFilePath = `${customOptions.fileDestination}/${customOptions.fileName}.txt`
+
+
     //Borrar carpeta antes de cada prueba
 
     beforeEach(()=>{
 
         fs.rmSync('outputs', {recursive:true,force:true})
 
+        fs.rmSync(`${customOptions.fileDestination}`, {recursive:true,force:true})
+
     })
 
     afterEach(()=>{
 
         fs.rmSync('outputs', {recursive:true,force:true})
+
+        fs.rmSync(`${customOptions.fileDestination}`, {recursive:true,force:true})
 
     })
     
@@ -32,4 +46,30 @@ describe('Test in UseCase Save File',()=>{
         expect(checkFile).toBe(true)
         expect(fileContent).toContain(options.fileContent)
     })
+
+
+    test('Should save file with custom values ', () => {
+      
+        const saveFile = new SaveFile();
+
+        /*const options = {
+            fileContent: 'custom content',
+            fileDestination:'custom-outputs',
+            fileName: 'custom-table-name'
+        }*/
+
+        /*const filePath = `${options.fileDestination}/${options.fileName}.txt`*/
+
+        const result = saveFile.execute(customOptions)
+        const checkFile = fs.existsSync(customFilePath)
+        const fileContent = fs.readFileSync(customFilePath,{encoding: "utf-8"})   
+
+
+        expect(result).toBe(true)
+        expect(checkFile).toBe(true)
+        expect(fileContent).toContain(customOptions.fileContent)
+
+    })
+    
+
 })
